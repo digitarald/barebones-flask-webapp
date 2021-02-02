73b8c0b0a1c3305644b80f6b2e9a332e38fc0de4
Steven Clarke <stevencl@users.noreply.github.com>
2021-02-02 11:48:00 +0000

    Hotfix

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/<user_name>')
+def hello_name(user_name):
+    return "Hello {}!".format(user_name)
+
 @app.route("/")
 def hello():
   return "Hello World!"
-
-@app.route('/<name>')
-def hello_name(user_name):
-    return "Hello {}!".format(user_name)
 
 if __name__ == "__main__":
   app.run()
